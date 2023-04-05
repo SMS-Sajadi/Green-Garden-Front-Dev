69bceaf0bec4647667370bbbc799c16aca0d7d1b
Soroush <82648856+SMS-Sajadi@users.noreply.github.com>
2023-04-05 20:30:18 +0330

    Making a simple User model
    * Making a simple User model :\
    
    * remove username
    
    ---------
    
    Co-authored-by: mohammad <9m.kh557@gmai.com>

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserBase(AbstractUser):
+    email = models.EmailField(max_length=200, unique=True)
+    city = models.CharField(max_length=50, null=True)
+    profile_photo = models.ImageField()
+    phone_number = models.IntegerField(null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
