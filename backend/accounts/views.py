04377340f58ba5936aac88b98d4651b85e02d792
mohammad <9m.kh557@gmai.com>
2023-04-07 01:59:02 +0330

    login

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm, VerifyCodeForm
@@ -26,7 +27,15 @@ class UserRegisterView(View):
 
             random_code = random.randint(1000, 9999)
             send_otp_code(cd['full_name'], cd['email'], random_code)
-            OtpCode.objects.create(email=cd['email'], code=random_code)
+
+            last_code = OtpCode.objects.filter(email=cd['email'])
+            if last_code:
+                last_code = last_code[0]
+                last_code.code = random_code
+                last_code.save()
+            else:
+                OtpCode.objects.create(email=cd['email'], code=random_code)
+
             request.session['user_registration_info'] = {
                 'phone_number': cd['phone'],
                 'email': cd['email'],
@@ -54,14 +63,38 @@ class UserRegisterVerifyCodeView(View):
             cd = form.cleaned_data
 
             if cd['code'] == code_instance.code:
-                User.objects.create_user(user_session['phone_number'], user_session['email'],
+                user = User.objects.create_user(user_session['phone_number'], user_session['email'],
                                          user_session['full_name'], user_session['password'])
 
                 code_instance.delete()
                 messages.success(request, 'you registered successfully', 'success')
+                login(request, user)
                 return redirect('home:home')
             else:
                 messages.error(request, 'entered cod is wrong', 'danger')
                 return redirect('accounts:verify_code')
 
+        # todo why redirect to home when form is not valid?
+        return redirect('home:home')
+
+
+class UserLoginView(View):
+    template_name = 'accounts/login.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        email = request.POST['email']
+        password = request.POST['pass']
+        user = User.objects.filter(email=email)
+
+        if not user:
+            return redirect('accounts:user_register')
+
+        user = user[0]
+        if not user.check_password(password):
+            return redirect('accounts:user_login')
+
+        login(request, user)
         return redirect('home:home')
