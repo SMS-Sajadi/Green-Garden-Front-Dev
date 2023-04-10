0b4d8eddfdc5c4ea6a569819bc03c439afc4ac96
mohammad <9m.kh557@gmai.com>
2023-04-10 21:43:18 +0330

    remove rest framework

--- backend/green_garden/settings.py
+++ backend/green_garden/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework',
     'accounts.apps.AccountsConfig',
     'home.apps.HomeConfig',
 ]
