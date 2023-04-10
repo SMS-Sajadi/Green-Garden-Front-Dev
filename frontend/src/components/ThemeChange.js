4f3ea27831213da459982263d3fc0bca5290b0ae
Mehrnaz shahi <91686230+mehrnaz-shahi@users.noreply.github.com>
2023-04-10 22:58:35 +0430

    fix ThemeChange.js bug

--- frontend/src/components/ThemeChange.js
+++ frontend/src/components/ThemeChange.js
@@ -11,23 +11,23 @@ const ThemeChange = () => {
 
     return (
     
-<div class={darkTheme ? "container" : "darkMode"  }>
-      <div class="sky" onClick={themeHandeler}>
+<div className={darkTheme ? "container" : "darkMode"  }>
+      <div className={"sky"} onClick={themeHandeler}>
         <img
-          class="moon moonrise"
+          className={"moon moonrise"}
           src="https://i.postimg.cc/B6cVZTXv/moon.png"
           width="70"
           height="70"
           alt=""
         />
-        <img class="sun" src="https://i.postimg.cc/XqjDrrCk/sun.png" width="100" height="100" alt="" />
+        <img className={"sun"} src="https://i.postimg.cc/XqjDrrCk/sun.png" width="100" height="100" alt="" />
         <img
-          class="clouds clouds-dark"
+          className={"clouds clouds-dark"}
           src="https://i.postimg.cc/rmLHLnyb/clouds-dark.png"
           width="70"
           alt=""
         />
-        <img class="clouds clouds-light" src="https://i.postimg.cc/Xv5PmSWd/clouds.png" width="70" alt="" />
+        <img className={"clouds clouds-light"} src="https://i.postimg.cc/Xv5PmSWd/clouds.png" width="70" alt="" />
       </div>
     </div>    );
 };
