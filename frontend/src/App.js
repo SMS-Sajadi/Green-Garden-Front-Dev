209a71a5221fe8ed36aa25be94de832183aa5e8f
Mehrnaz shahi <91686230+mehrnaz-shahi@users.noreply.github.com>
2023-04-10 22:36:13 +0430

    edit and adding route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
-import {Routes, Route } from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
-
-
+import ConfirmCode from './components/ConfirmCode';
 
 function App() {
   return (
     <div>
           <Routes >
             <Route path='/login'  element={<Login />}/>
+            <Route path='/home'  element={<h1>Home page</h1>}/>
+
+            <Route path='/signUp/confirm' element={<ConfirmCode />}/>
             <Route path='/signUp'  element={<SignUp />}/>
+            <Route path='/'  element={<Navigate to={'/signUp'} />}/>
+
+
+
         </Routes>
 
 
--- frontend/src/components/ConfirmCode.js
+++ frontend/src/components/ConfirmCode.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import {notify} from './toast.js';
  import VerificationInput from "react-verification-input";
 import axios from 'axios';
 
 
 const ConfirmCode = () => {
 
+    const navigate = useNavigate();
+
 
     const handleCode = (event) =>{
+        let res;
+
         axios.post('http://localhost:8000/accounts/verify', event )
-            .then(response => console.log(response) );
+            .then(response => res = response.is_ok );
 
-
+        if(res){
+            notify('با موفقیت ثبت نام شدید', 'success');
+            navigate("/login");
+        }
+        else{
+            console.log('again')
+        }
 
     }
 
@@ -19,18 +31,19 @@ const ConfirmCode = () => {
     return (
         <div>
             <VerificationInput
-                classNames={{
-                    container: "container",
-                    character: "character",
-                    characterInactive: "character--inactive",
-                    characterSelected: "character--selected",
-                  }}
+                // classNames={{
+                //     container: "container",
+                //     character: "character",
+                //     characterInactive: "character--inactive",
+                //     characterSelected: "character--selected",
+                //   }}
                   length = {4}
                   validChars = {'0-9'}
                   placeholder = {'_'}
                   autoFocus = {true}
                   onComplete = {handleCode}
                     />
+
 
         </div>
     );
